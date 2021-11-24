15977c3584ed6208b4cc23e9ba691227a8f54b3b
xuseto <jesniehe85@gmail.com>
2021-11-24 18:52:11 +0100

    create system files

---
+++ Src/system/system_api.h
@@ -0,0 +1,47 @@
+/***************************************************************************************************
+ * @file system_api.h 
+ * @author jnieto
+ * @version 1.0.0.0.0 
+ * @date Creation: 24/11/2021
+ * @date Last modification 24/11/2021 by jnieto
+ * @brief All System of the Kart
+ * @par
+ *  COPYRIGHT NOTICE: (c) jnieto
+ *  All rights reserved
+ ***************************************************************************************************
+
+    @addtogroup SYSTEM
+    @{
+    @defgroup SYSTEM_API  SYSTEM object
+    @{
+    @brief
+    @details
+
+***************************************************************************************************/
+
+/* Define to prevent recursive inclusion ---------------------------------------------------------*/
+#ifndef __SYSTEM_API_H
+#define __SYSTEM_API_H
+
+/* Includes --------------------------------------------------------------------------------------*/
+#include <def_common.h>
+
+/* Typedefs --------------------------------------------------------------------------------------*/
+
+/* Public Function -------------------------------------------------------------------------------*/
+/**
+ * @brief Init all system of the kart
+ * 
+ */
+void system_init (void);
+
+#endif /* __SYSTEM_API_H */
+
+/**
+  * @}
+  */
+/**
+  * @}
+  */
+
+/************************* (C) COPYRIGHT ****** END OF FILE ***************************************/
