721dc7b623aaa76608d40dffc1f0a458a412674a
xuseto <jesniehe85@gmail.com>
2022-03-06 18:32:57 +0100

    created files sevcon

---
+++ Src/sevcon/sevcon_config.h
@@ -0,0 +1,36 @@
+/***************************************************************************************************
+ * @file sevcon_config.h
+ * @author jnieto
+ * @version 1.0.0.0.0
+ * @date Creation: 06/03/2022
+ * @date Last modification 06/03/2022 by jnieto
+ * @brief SEVCON
+ * @par
+ *  COPYRIGHT NOTICE: (c) jnieto
+ *  All rights reserved
+ ***************************************************************************************************
+
+    @defgroup SEVCON_CONFIG  SEVCON object
+    @{
+    @brief
+    @details
+
+***************************************************************************************************/
+
+/* Define to prevent recursive inclusion ---------------------------------------------------------*/
+#ifndef __SEVCON_CONFIG_H
+#define __SEVCON_CONFIG_H
+
+/* Includes --------------------------------------------------------------------------------------*/
+
+/* Defines ---------------------------------------------------------------------------------------*/
+/** MAX of functions callback regsiter every SEVCON */
+#define MAX_SEVCON_CLLBCK 3
+
+#endif /* __SEVCON_CONFIG_H */
+
+/**
+ * @}
+ */
+
+/************************* (C) COPYRIGHT ****** END OF FILE ***************************************/
